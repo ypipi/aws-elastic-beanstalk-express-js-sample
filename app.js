46859ee64a883d93c695afb0d2c795419a4291aa
Y I <yi@Ys-MacBook-Air.local>
2020-12-06 17:41:57 -0500

    Full pipeline test

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hey Yukiko!'));
+app.get('/', (req, res) => res.send('The message has been changed.'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
